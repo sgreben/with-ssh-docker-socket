ea59ac50d6601aed14eaaa13decfbdfdc5cb809a
Sergey Grebenshchikov <sgreben@gmail.com>
2018-12-13 05:55:03 +0100

    Release 1.2.2

--- conn_pipe.go
+++ conn_pipe.go
@@ -2,36 +2,22 @@ package main
 
 import (
 	"io"
-	"log"
 	"net"
 	"sync"
 )
 
 func connPipe(local, remote net.Conn) {
 	var wg sync.WaitGroup
-	var shutdown bool
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		if _, err := io.Copy(local, remote); err != nil {
-			if shutdown {
-				return
-			}
-			log.Printf("copy remote->local: %v", err)
-		}
-		shutdown = true
+		io.Copy(local, remote)
 		local.Close()
 	}()
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		if _, err := io.Copy(remote, local); err != nil {
-			if shutdown {
-				return
-			}
-			log.Printf("copy local->remote: %v", err)
-		}
-		shutdown = true
+		io.Copy(remote, local)
 		remote.Close()
 	}()
 	wg.Wait()
